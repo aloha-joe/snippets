05d820b309fb3a758278b8015c01ada04074e392
Alexey Kuimov <elvingeeboo@yandex.ru>
2022-08-30 22:07:15 +0300

    [SF-14] - Добавлено решение

--- tests/algos/bubble_sort/test_bubble_sort.py
+++ tests/algos/bubble_sort/test_bubble_sort.py
@@ -3,8 +3,7 @@ from algos import bubble_sort
 
 test_list = [432, 6453, 312, 551, 1]
 bubbled = bubble_sort.sort(test_list)
-clean_sorted = sorted(test_list, reverse=True)
-if bubbled == clean_sorted:
+if bubbled == [6453, 551, 432, 312, 1]:
     print("Correct")
 else:
     print("Incorrect")
