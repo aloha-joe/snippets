b8670bb24b77cdf68c614d5417e3083159b078fe
Alexey Kuimov <elvingeeboo@yandex.ru>
2022-08-30 22:05:24 +0300

    [SF-14] - Добавлено решение

--- algos/bubble_sort.py
+++ algos/bubble_sort.py
@@ -1,7 +1,6 @@
-from numpy import *
 def sort(a):
     s = 0
-    for i in range(len(a) - 1):
+    for _ in range(len(a) - 1):
         for i in range(len(a) - 1):
             while a[i] < a[i + 1]:
                 s = a[i]
--- tests/algos/bubble_sort/test_bubble_sort.py
+++ tests/algos/bubble_sort/test_bubble_sort.py
@@ -1,9 +1,9 @@
 import random
 from algos import bubble_sort
 
-test_list = [random.randint (1,1000) for i in range(5)]
+test_list = [432, 6453, 312, 551, 1]
 bubbled = bubble_sort.sort(test_list)
-clean_sorted = sorted(test_list, reverse= True)
+clean_sorted = sorted(test_list, reverse=True)
 if bubbled == clean_sorted:
     print("Correct")
 else:
